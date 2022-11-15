3004a6f9d0383654970f8a2566cec0217c7b47f7
tenu2022 <tenu2022@gmail.com>
2022-11-15 11:45:24 +0800

    modify

--- index.py
+++ index.py
@@ -1,29 +1,32 @@
 import streamlit as st
 import time
-
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import json
+import pandas as pd
 
 # Use a service account.
-app = None
-db = None
-if db is None:
-    print("建立firestore實體")
+if 'firestore' not in st.session_state:
+    st.session_state['firestore'] = True   
     key_dict = json.loads(st.secrets["textkey"])
-    cred = credentials.Certificate(key_dict)
-    if app is None:
-        app = firebase_admin.initialize_app(cred)
+    cred = credentials.Certificate(key_dict)   
+    app = firebase_admin.initialize_app(cred)
     db = firestore.client()
-
-doc_ref = db.collection('records')
-docs = doc_ref.get()
-for doc in docs:
-    print(doc)
+    st.session_state['db'] = db
 
 
-st.title("光線和距離即時監測")
+db = st.session_state['db']
+records_ref = db.collection('records')
+query = records_ref.limit_to_last(20)
+docs = query.get()
+
+
+datalist = [doc.to_dict() for doc in docs]
+dict_data = pd.DataFrame(datalist)
+st.title("光線和距離及時監控")
+st.table(dict_data)
+
 
 
 def downloadData():
