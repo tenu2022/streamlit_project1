dca1b7632998de89c1770b9087cacc6be89834ae
tenu2022 <tenu2022@gmail.com>
2022-11-15 10:21:35 +0800

    modify

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import time
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account.
+cred = credentials.Certificate('private/raspberry1-47370-firebase-adminsdk-9eb4u-92da494d53.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 st.title("光線和距離即時監測")
 
