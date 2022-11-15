b41f36af3400b3d3a00359cf39c8c60ac44598ec
tenu2022 <tenu2022@gmail.com>
2022-11-15 10:40:37 +0800

    modify

--- index.py
+++ index.py
@@ -4,17 +4,22 @@ import time
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
 
 # Use a service account.
-cred = credentials.Certificate('private/raspberry1-47370-firebase-adminsdk-9eb4u-92da494d53.json')
+app = None
+db = None
+if db is None:
+    print("建立firestore實體")
+    key_dict = json.loads(st.secrets["textkey"])
+    cred = credentials.Certificate(key_dict)
+    if app is None:
+        app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
 
-app = firebase_admin.initialize_app(cred)
 
-db = firestore.client()
 
 st.title("光線和距離即時監測")
-
-
 
 
 def downloadData():
